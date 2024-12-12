4a909a9ec1b3ad471cdfa05185a8aa54b78497e9
观澜听雨 <tangxiaochuane@yeah.net>
2024-12-12 11:08:23 +0800

    iplement the welecome page and custom button conponents

--- app/(auth)/welecome.tsx
+++ app/(auth)/welecome.tsx
@@ -1,13 +1,15 @@
+import CustomButton from "@/components/Biu/CustomButton";
 import { onboarding } from "@/constances";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
 const Welecome = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const swiperRef = useRef<Swiper>(null);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -36,12 +38,31 @@ const Welecome = () => {
       >
         {onboarding.map((item) => (
           <View>
-            <Text>{item.title}</Text>
-            {/* <Text>{item.description}</Text> */}
-            {/* <Text>{item.title}</Text> */}
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-in")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
